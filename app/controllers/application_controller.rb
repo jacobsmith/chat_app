877ce336055fff12868e0a860278ce005c883996
Jacob Smith <jacob.wesley.smith@gmail.com>
2018-03-26 20:51:37 -0400

    that was the wrong way to try and skip...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
-  skip_before_action :verify_authenticity_token
 
   def current_user
     User.find_by(session_token: cookies[:session_token])
