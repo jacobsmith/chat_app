ee16113526f1bacf66e4e2cc89d3867d9d7c1577
Jacob Smith <jacob.wesley.smith@gmail.com>
2018-05-03 19:05:04 -0400

    back to all for cors

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module ChatApp
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'true'
+        origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options, :delete]
       end
     end
